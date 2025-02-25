5e3c14d13d221c846ab8163ca9fe6426ea379765
Ryan Garcia <ryankgarcia22@gmail.com>
2025-02-25 22:52:33 +0000

    created a functioning event listener for the photo URL.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,10 @@
+'use strict';
 // create an interface for the data object //
-
 const $photoURL = document.querySelector('#photoURL');
-
 if (!$photoURL) throw new Error('$photoURL query failed');
-
-function handlePhotoUrl(event: Event): void {
-  const $photoId = document.getElementById('photo') as HTMLImageElement;
-  const $eventTarget = event.target as HTMLInputElement;
+function handlePhotoUrl(event) {
+  const $photoId = document.getElementById('photo');
+  const $eventTarget = event.target;
   $photoId.src = $eventTarget.value;
 }
-
 $photoURL.addEventListener('input', handlePhotoUrl);
--- ts/data.ts
+++ ts/data.ts
@@ -1,4 +1,11 @@
-const data = {
+interface Data {
+  view: string;
+  entries: []; // fix this
+  editing: null;
+  nextEntryId: number;
+}
+
+const data: Data = {
   view: 'entry-form',
   entries: [],
   editing: null,
