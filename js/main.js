67392b3bff015a62147d3157cfa79d9577083d7e
Ryan Garcia <ryankgarcia22@gmail.com>
2025-02-26 23:37:19 +0000

    added new object to beginning of the data model, assigned an entryId, prevent the page from refreshing, store form values.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 'use strict';
-// create an interface for the data object //
 const $photoURL = document.querySelector('#photoURL');
 if (!$photoURL) throw new Error('$photoURL query failed');
 function handlePhotoUrl(event) {
@@ -8,3 +7,22 @@ function handlePhotoUrl(event) {
   $photoId.src = $eventTarget.value;
 }
 $photoURL.addEventListener('input', handlePhotoUrl);
+const $form = document.querySelector('form');
+if (!$form) throw new Error('$form query failed');
+$form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const $formElements = $form.elements;
+  if (!$formElements) throw new Error('$formElements query failed');
+  const newEntry = {
+    entryId: data.nextEntryId,
+    title: $formElements.title.value,
+    photoUrl: $formElements.photoURL.value,
+    notes: $formElements.textarea.value,
+  };
+  data.nextEntryId++;
+  // console.log('before unshift method', data.entries);
+  data.entries.unshift(newEntry);
+  // console.log('after unshift method', data.entries);
+  $form.reset();
+  $formElements.photoURL.value = '/images/placeholder-image-square.jpg';
+});
--- ts/main.ts
+++ ts/main.ts
@@ -1,5 +1,3 @@
-// create an interface for the data object //
-
 const $photoURL = document.querySelector('#photoURL');
 
 if (!$photoURL) throw new Error('$photoURL query failed');
@@ -11,3 +9,46 @@ function handlePhotoUrl(event: Event): void {
 }
 
 $photoURL.addEventListener('input', handlePhotoUrl);
+
+// below is the event listener for the submit button //
+
+interface Entry {
+  title: string;
+  photoUrl: string;
+  notes: string;
+  entryId: number;
+}
+
+interface FormElements extends HTMLFormControlsCollection {
+  title: HTMLInputElement;
+  photoURL: HTMLInputElement;
+  textarea: HTMLTextAreaElement;
+}
+
+const $form = document.querySelector('form');
+
+if (!$form) throw new Error('$form query failed');
+
+$form.addEventListener('submit', (event: Event): void => {
+  event.preventDefault();
+
+  const $formElements = $form.elements as FormElements;
+
+  if (!$formElements) throw new Error('$formElements query failed');
+
+  const newEntry: Entry = {
+    entryId: data.nextEntryId,
+    title: $formElements.title.value,
+    photoUrl: $formElements.photoURL.value,
+    notes: $formElements.textarea.value,
+  };
+
+  data.nextEntryId++;
+
+  // console.log('before unshift method', data.entries);
+  data.entries.unshift(newEntry);
+  // console.log('after unshift method', data.entries);
+
+  $form.reset();
+  $formElements.photoURL.value = '/images/placeholder-image-square.jpg';
+});
