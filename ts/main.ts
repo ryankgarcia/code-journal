78f2910972d30735b98f60c8bdccb9258a41856b
Ryan Garcia <ryankgarcia22@gmail.com>
2025-02-27 02:31:24 +0000

    Checked for errors, tested functionality, deleted CSS classes that were not being used.

--- ts/main.ts
+++ ts/main.ts
@@ -12,12 +12,12 @@ interface FormElements extends HTMLFormControlsCollection {
 }
 const $photoId = document.getElementById('photo') as HTMLImageElement;
 
-// the function is looking to see when the event that a user pastes in a
-// new url in the URL, and updates it based on the link that is pasted
-
 const $photoURL = document.querySelector('#photoURL');
 
 if (!$photoURL) throw new Error('$photoURL query failed');
+
+// the function is looking to see when the event that a user pastes in a
+// new url in the URL, and updates it based on the link that is pasted
 
 function handlePhotoUrl(event: Event): void {
   const $eventTarget = event.target as HTMLInputElement;
@@ -26,13 +26,13 @@ function handlePhotoUrl(event: Event): void {
 
 $photoURL.addEventListener('input', handlePhotoUrl);
 
-// the code below this queries the form on my DOM, if its not found, throw new Error
+// the code below this queries the form on the DOM, if its not found, throw new Error
 
 const $form = document.querySelector('form');
 
 if (!$form) throw new Error('$form query failed');
 
-// this code below is an event listener callback function that checks our dom each
+// this code below is an event listener callback function that checks the dom each
 // time a form is submitted, and adds a new entry to local storage
 // with each new entry that is added
 
